73f16920bde07a9d2f6ff142c28420fb862b161f
justinbrown11 <96081581+justinbrown11@users.noreply.github.com>
2022-10-27 18:41:12 -0600

    start documentation

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# utahvalleypickleball
+# Welcome to utahvalleypickleball.com (hopefully)!
+
+App Name : utahvalleypickleball
+
+Development Team (Team 4):
+
+- Justin Brown
+- Rachel Daniel
+- Brooke Woodland
+- Ellie Richardson
+
+## App Description:
+Site for pickleball (will add more later)
--- pickleball/views.py
+++ pickleball/views.py
@@ -1,13 +1,25 @@
 from django.http import HttpResponse
 
 def indexPageView(request):
+    """
+    This is the landing page
+    """
     return HttpResponse('Homepage')
 
 def aboutPageView(request):
+    """
+    This is the about page
+    """
     return HttpResponse('About Page')
 
 def mapsPageView(request):
+    """
+    This is the maps page
+    """
     return HttpResponse('Map Page')
 
 def dataPageView(request):
+    """
+    This is the data page
+    """
     return HttpResponse('Data Page')
