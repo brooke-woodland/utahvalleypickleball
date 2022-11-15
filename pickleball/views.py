053e346b3edf8333b9f00561af1470d3c8358608
justinbrown11 <justin.j.brown@outlook.com>
2022-11-15 13:31:28 -0700

    added testing map

--- pickleball/views.py
+++ pickleball/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import googlemaps
+# import asyncio
+from json import dumps
 
 def indexPageView(request):
     """
@@ -17,7 +20,43 @@ def mapsPageView(request):
     """
     This is the maps page
     """
-    return render(request, 'pickleball/map.html')
+    gmaps = googlemaps.Client(key="AIzaSyA1QLwVy_gy2FPUh4v6_ly1CqmtUkk7IAw")
+
+    sampleData = [
+        {
+            "address": "600 North 300 East",
+            "city": "Orem",
+            "state": "UT"
+        },
+        {
+            "address": "575 W Center St",
+            "city": "Orem",
+            "state": "UT"
+        },
+        {
+            "address": "320 500 N",
+            "city": "Provo",
+            "state": "UT"
+        },
+    ]
+
+    coordinateArray = []
+
+    for data in sampleData:
+        geocode_result = gmaps.geocode(f"{data['address']}, {data['city']}, {data['state']}")
+
+        latitude = geocode_result[0]['geometry']['location']['lat']
+        longitude = geocode_result[0]['geometry']['location']['lng']
+
+        coordinateArray.append({ "lat": latitude, "lng": longitude })
+
+    coordinateArray = dumps(coordinateArray)
+
+    context = {
+        "data": coordinateArray
+    }
+
+    return render(request, 'pickleball/map.html', context)
 
 def dataPageView(request):
     """
