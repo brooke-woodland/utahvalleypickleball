57006377086d568af957422485c870cfa77e7771
wb385334 <brooke@woodlandfamily.org>
2022-11-15 17:43:20 -0700

    default for open and close time

--- pickleball/models.py
+++ pickleball/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 
+
 # Create your models here.
 class Location(models.Model):
     court_name = models.CharField(max_length=50)
@@ -8,8 +9,8 @@ class Location(models.Model):
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=2, default='UT')
     courts = models.IntegerField(default=0)
-    openTime = models.TimeField(default=0)
-    closeTime = models.TimeField(default=0)
+    openTime = models.TimeField(default= None)
+    closeTime = models.TimeField(default= None)
     indoor = models.BooleanField(default=False)
         
 
