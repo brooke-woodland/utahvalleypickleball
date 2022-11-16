8d12bcdeb3d60527a958adcf6744cdb8584bd796
wb385334 <brooke@woodlandfamily.org>
2022-11-15 17:25:37 -0700

    force UT as state

--- pickleball/models.py
+++ pickleball/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.core.exceptions import ValidationError
 
 # Create your models here.
-class Location(models.Model):
+class location(models.Model):
     court_name = models.CharField(max_length=50)
     street_address = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
-    state = models.CharField(default='UT')
+    state = models.CharField(max_length=2, default='UT')
     courts = models.IntegerField(default=0)
-    OpenTime = models.TimeField(default=0)
-    CloseTime = models.TimeField(default=0)
-    Indoor = models.BooleanField
+    openTime = models.TimeField(default=0)
+    closeTime = models.TimeField(default=0)
+    indoor = models.BooleanField
         
 
